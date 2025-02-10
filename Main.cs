d4b42275e04b5ec8fb242b28c0f63e7c52104133
Nick Fox <66554932+NickFox007@users.noreply.github.com>
2025-02-10 22:52:52 +0300

    2.0

--- Main.cs
+++ Main.cs
@@ -1,71 +1,134 @@
-﻿using CounterStrikeSharp.API.Core.Capabilities;
-using VipCoreApi;
-using Hooks;
+﻿
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using static VipCoreApi.IVipCoreApi;
-using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
-using Microsoft.Extensions.Logging;
-using System.Numerics;
+using CounterStrikeSharp.API.Core.Capabilities;
+using HGR;
+using VipCoreApi;
+using static VipCoreApi.IVipCoreApi;
 
-
-namespace VipHooks;
-public class VipHooks(): BasePlugin
+namespace VipHGR;
+public class VipHGR : BasePlugin
 {
-    
-
     public override string ModuleAuthor => "Nick Fox";
-    public override string ModuleName => "[VIP] Hooks";
-    public override string ModuleVersion => "1.2";
+    public override string ModuleName => "[VIP] HGR";
+    public override string ModuleVersion => "2.0";
 
     private IVipCoreApi? _vip;
-    private IHooksApi? _hooks;
+    private IHGRApi? _hgr;
+
     private HooksModule _viphooks;
+    private HooksModuleEnd _viphooksend;
+    private GrabsModule _vipgrabs;
+    private GrabsModuleEnd _vipgrabsend;
+    private RopesModule _vipropes;
+    private RopesModuleEnd _vipropesend;
+
     private PluginCapability<IVipCoreApi> PluginVip { get; } = new("vipcore:core");
-    private PluginCapability<IHooksApi> PluginHooks { get; } = new("hooks:nfcore");
+    private PluginCapability<IHGRApi> PluginHooks { get; } = new("hgr:nfcore");
+
+    
 
     public override void OnAllPluginsLoaded(bool hotReload)
     {
         _vip = PluginVip.Get();
-        _hooks = PluginHooks.Get();
+        _hgr = PluginHooks.Get();
 
-        if (_vip == null || _hooks == null) return;
+        hgrCount = new int[3][];
+        for (int i = 0; i < 3; i++)
+            hgrCount[i] = new int[65];
 
-        _vip.OnCoreReady += () =>
-        {
-            OnCoreLoaded();
-        };
+        if (_vip == null || _hgr == null) return;
+
+        _vip.OnCoreReady += OnCoreLoaded;
 
         if (hotReload)
-            OnCoreLoaded();
+            OnCoreLoaded();        
     }
 
+    public override void Unload(bool hotReload)
+    {
+        _hgr.RemHook(HGRHook);
+    }
 
-    private int[] hooksCount = new int[65];
+    private int[][] hgrCount; // 0 - Hooks, 1 - Grabs, 2 - Ropes
+
+    private void OnCoreLoaded()
+    {
+        _viphooks = new HooksModule(_vip);
+        _viphooksend = new HooksModuleEnd(_vip);
+        _vipgrabs = new GrabsModule(_vip);
+        _vipgrabsend = new GrabsModuleEnd(_vip);
+        _vipropes = new RopesModule(_vip);
+        _vipropesend = new RopesModuleEnd(_vip);
+
+        _vip.RegisterFeature(_viphooks, FeatureType.Hide);
+        _vip.RegisterFeature(_viphooksend, FeatureType.Hide);
+        _vip.RegisterFeature(_vipgrabs, FeatureType.Hide);
+        _vip.RegisterFeature(_vipgrabsend, FeatureType.Hide);
+        _vip.RegisterFeature(_vipropes, FeatureType.Hide);
+        _vip.RegisterFeature(_vipropesend, FeatureType.Hide);
+
+        _hgr.AddHook(HGRHook, 50);
+
+    }
+
+    private void HGRHook(PlayerHGR info)
+    {
+        if(info.State() == HGRState.Disabled)
+        {
+            int i = 0;
+            switch(info.Mode())
+            {
+                case HGRMode.Hook: i = 0; break;
+                case HGRMode.Grab: i = 1; break;
+                case HGRMode.Rope: i = 2; break;
+                default: return;
+            }
+
+            if (hgrCount[i][info.Player().Slot] == -1)
+                info.Enable();
+            else
+                if(hgrCount[i][info.Player().Slot] > 0)
+                {                    
+                    hgrCount[i][info.Player().Slot]--;
+                    info.Enable();
+
+                    if (hgrCount[i][info.Player().Slot] == 0)
+                        _vip.PrintToChat(info.Player(), Localizer["vip_hgr.expired"]);
+                    else
+                        _vip.PrintToChat(info.Player(), String.Format(Localizer["vip_hgr.use_count"], hgrCount[i][info.Player().Slot]));
+                }
+        }
+    }
+    
+
     [GameEventHandler]
     public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
-    {
-        for (int i = 0; i < 65; i++)
-            hooksCount[i] = 0;
+    {        
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 65; j++)
+                hgrCount[i][j] = 0;
         return HookResult.Continue;
     }
+
 
     [GameEventHandler]
     public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
     {
-        for (int i = 0; i < 65; i++)
+        foreach (var player in Utilities.GetPlayers())
         {
-            var player = Utilities.GetPlayerFromSlot(i);
+            var slot = player.Slot;
 
-            if (IsValidPlayer(player) && _vip.IsClientVip(player) && _vip.PlayerHasFeature(player, "HooksEnd"))
-            {
-                var count = _vip.GetFeatureValue<int>(player, "HooksEnd");
-                if (count == 0)
-                    hooksCount[i] = -1;
-                else
-                    hooksCount[i] = count;
-            }
+            for(int i = 0; i < 3; i++)
+                if (_vip.IsClientVip(player) && _vip.PlayerHasFeature(player, HGR.NamesEnd[i]))
+                {
+                    var count = _vip.GetFeatureValue<int>(player, HGR.NamesEnd[i]);
+                    if (count == -1)
+                        hgrCount[i][slot] = -1;
+                    else if (hgrCount[i][slot] != -1)
+                        hgrCount[i][slot] += count;
+                }
         }
         return HookResult.Continue;
     }
@@ -74,79 +137,22 @@ public class VipHooks(): BasePlugin
     [GameEventHandler]
     public HookResult OnFreezeEnd(EventRoundFreezeEnd @event, GameEventInfo info)
     {
-        for (int i = 0; i < 65; i++)
+        foreach (var player in Utilities.GetPlayers())
         {
-            var player = Utilities.GetPlayerFromSlot(i);
+            var slot = player.Slot;
 
-            if (IsValidPlayer(player) && _vip.IsClientVip(player) && _vip.PlayerHasFeature(player, "Hooks"))
-            {
-                var count = _vip.GetFeatureValue<int>(player, "Hooks");
-                if(count == 0)
-                    hooksCount[i] = -1;
-                else
-                    hooksCount[i] = count;
-            }
+            for (int i = 0; i < 3; i++)
+                if (_vip.IsClientVip(player) && _vip.PlayerHasFeature(player, HGR.Names[i]))
+                {
+                    var count = _vip.GetFeatureValue<int>(player, HGR.Names[i]);
+                    if (count == -1)
+                        hgrCount[i][slot] = -1;
+                    else if (hgrCount[i][slot] != -1)
+                        hgrCount[i][slot] += count;
+                }
         }
         return HookResult.Continue;
     }
 
-
-    private void OnCoreLoaded()
-    {
-        _viphooks = new HooksModule(_vip);
-        _vip.RegisterFeature(_viphooks, FeatureType.Hide);
-
-        _hooks.AddHook((playerinfo) => {
-
-            var slot = playerinfo.Player().Slot;
-            if (hooksCount[slot] > 0)
-            {
-                hooksCount[slot]--;
-                if (hooksCount[slot] == 0)
-                    _vip.PrintToChat(playerinfo.Player(), Localizer["expired"]);
-                else
-                    _vip.PrintToChat(playerinfo.Player(), String.Format(Localizer["use_count"], hooksCount[slot]));
-                playerinfo.Set(HookState.Enabled);
-                
-            }
-            else 
-                if(hooksCount[slot] == -1)
-                    playerinfo.Set(HookState.Enabled);           
-
-        });
-
-    }
-
-    private bool IsValidPlayer(CCSPlayerController player)
-    {
-        return player != null && player.IsValid && player.Connected == PlayerConnectedState.PlayerConnected && !player.IsBot;
-    }
-
-    public override void Unload(bool hotReload)
-    {
-        _vip.UnRegisterFeature(_viphooks);
-    }
-
-
-    public class HooksModule : VipFeatureBase
-    {
-        public override string Feature => "Hooks";                
-                            
-        public HooksModule(IVipCoreApi api) : base(api)
-        {
-            
-        }
-
-    }
-
-    public class HooksModuleEnd : VipFeatureBase
-    {
-        public override string Feature => "HooksEnd";
-
-        public HooksModuleEnd(IVipCoreApi api) : base(api)
-        {
-
-        }
-
-    }
+    
 }
---
+++ VipModules.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VipCoreApi;
+
+namespace VipHGR;
+
+public static class HGR
+{
+    public static string[] Names = ["Hooks", "Grabs", "Ropes"];
+    public static string[] NamesEnd = ["HooksEnd", "GrabsEnd", "RopesEnd"];
+}
+    
+
+public class HooksModule : VipFeatureBase
+{    public override string Feature => HGR.Names[0];
+    public HooksModule(IVipCoreApi api) : base(api) { }
+}
+
+public class HooksModuleEnd : VipFeatureBase
+{
+    public override string Feature => HGR.NamesEnd[0];
+    public HooksModuleEnd(IVipCoreApi api) : base(api) { }
+}
+
+public class GrabsModule : VipFeatureBase
+{
+    public override string Feature => HGR.Names[1];
+    public GrabsModule(IVipCoreApi api) : base(api) { }
+}
+
+public class GrabsModuleEnd : VipFeatureBase
+{
+    public override string Feature => HGR.NamesEnd[1];
+    public GrabsModuleEnd(IVipCoreApi api) : base(api) { }
+}
+
+public class RopesModule : VipFeatureBase
+{
+    public override string Feature => HGR.Names[2];
+    public RopesModule(IVipCoreApi api) : base(api) { }
+}
+
+public class RopesModuleEnd : VipFeatureBase
+{
+    public override string Feature => HGR.NamesEnd[2];
+    public RopesModuleEnd(IVipCoreApi api) : base(api) { }
+}
